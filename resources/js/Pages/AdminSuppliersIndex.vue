b62b7809210655e4b1129dfb303f52bf8bd91a62
Mark Evans <mevans@projectpay.com.au>
2025-01-30 18:09:14 +1100

    WIP

--- resources/js/Pages/AdminSuppliersIndex.vue
+++ resources/js/Pages/AdminSuppliersIndex.vue
@@ -178,7 +178,7 @@
                                             v-model="formSupplierCreate.name"
                                             type="text"
                                             class="w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                                            placeholder="Name"
+                                            placeholder="Business Name"
                                             required
                                             @input="autoComplete()"
                                         >
@@ -225,7 +225,7 @@
 
                                 <!-- auto suggestions-->
                                 <div>
-                                    <p v-if="autoSuggestionsExactMatch" class="mt-2 text-orange-500 text-left">
+                                    <p v-if="autoSuggestionsExactMatch && formSupplierCreate.name.length >= 3" class="mt-2 text-orange-500 text-left">
                                         Supplier exists
                                     </p>
 
